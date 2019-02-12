5023f0714a70203cc4366cb1e2762d94fc5d6e93
yangdepp <965093975@qq.com>
2019-02-12 18:21:51 +0800

    添加icon

--- readme.md
+++ readme.md
@@ -4,3 +4,6 @@
 ## 安装
 
 使用本框架前，请在 css 中开启 border-box
+
+运行项目：
+npx parcel index.html --no-cache
--- src/button.vue
+++ src/button.vue
@@ -1,27 +1,62 @@
 <template>
-  <button class="g-button">按钮</button>
+  <button class="g-button" :class="{[`icon-${iconPosition}`]: true}">
+    <svg class="icon" v-if="icon">
+      <use :xlink:href="`#i-${icon}`"></use>
+    </svg>
+    <div class="content">
+      <slot></slot>
+    </div>
+  </button>
 </template>
 <script>
   export default {
     // name: "g-button"
+    props: ['icon', 'iconPosition']
   };
 </script>
 <style lang="scss">
   .g-button {
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    vertical-align: middle;
+    cursor: pointer;
     font-size: var(--font-size);
     height: var(--button-height);
     padding: 0 1em;
     border-radius: var(--border-radius);
     border: 1px solid var(--border-color);
     background: var(--button-bg);
+
     &:hover {
       border-color: var(--border-color-hover);
     }
+
     &:active {
       background-color: var(--button-active-bg);
     }
+
     &:focus {
       outline: none;
+    }
+
+    > .icon {
+      order: 1;
+      margin-right: .3em;
+    }
+
+    > .content {
+      order: 2;
+    }
+    &.icon-right {
+      > .icon {
+        order: 2;
+        margin-left: .3em;
+        margin-right: 0;
+      }
+      > .content{
+        order: 1;
+      }
     }
   }
 </style>
